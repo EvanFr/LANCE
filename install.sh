9bea69022101a36b5566a26b9f7b7dc6b283b00f
EvanFr <v.froudakis@gmail.com>
2025-09-12 12:27:50 -0400

    Updating install.sh to handle the UI_env creation.

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ echo "Unzipping completed. [1/6]"
 cp ./artifact/LANCE_sota_LLMs/Llama/reports.txt ./artifact/LANCE_sota_LLMs/Gemini/reports.txt
 
 sed -i '$d' ./artifact/LANCE_sota_LLMs/transformers_env.yml
-echo "    - iocsearcher==2.4.3" >> artifact/LANCE_sota_LLMs/transformers_env.yml
+echo "      - iocsearcher==2.4.3" >> artifact/LANCE_sota_LLMs/transformers_env.yml
 # Making sure Anaconda is installed
 
 if command -v conda &> /dev/null; then
@@ -38,7 +38,7 @@ fi
 echo "Creating conda environment for UI (claim 2)..."
 eval "$(conda shell.bash hook)"
 
-conda env create -n UI_env
+conda env create -n UI_env python=3.12 -y
 conda activate UI_env
 pip install -r ./artifact/UI/requirements.txt
 conda deactivate
@@ -48,26 +48,26 @@ echo "Conda environment for UI created. [2/6]"
 # Create LANCE-GPT conda environment
 echo "Creating conda environment for LANCE-GPT (claim 5)..."
 
-conda env create -f ./artifact/LANCE/environment.yaml
+conda env create -f ./artifact/LANCE/environment.yaml -y
 
 echo "Conda environment for LANCE-GPT created. [3/6]"
 
 # Create LANCE-OpenSource conda environment
 echo "Creating conda environment for LANCE-OpenSource (claim 6)..."
 
-conda env create -f ./artifact/LANCE_sota_LLMs/transformers_env.yml
+conda env create -f ./artifact/LANCE_sota_LLMs/transformers_env.yml  -y
 echo "Conda environment for LANCE-OpenSource created. [4/6]"
 
 # Create LANCE-Gemini conda environment
 echo "Creating conda environment for LANCE-Gemini (claim 6)..."
 
-conda env create -f ./artifact/LANCE_sota_LLMs/Gemini/environment.yaml
+conda env create -f ./artifact/LANCE_sota_LLMs/Gemini/environment.yaml -y
 echo "Conda environment for LANCE-Gemini created. [5/6]"
 
 # Create IoC_Miner conda environment
 echo "Creating conda environment for IoC_Miner (claim 9)..."
 
-conda env create -f ./artifact/IoCMiner/environment.yaml
+conda env create -f ./artifact/IoCMiner/environment.yaml -y
 
 echo "Conda environment for IoC_Miner created. [6/6]"
 
